9f64e38edf7ff2cb641ff1e078f97b3b273f2784
Dustin Specker <dustinspecker@dustinspecker.com>
2015-10-06 18:05:07 -0500

    test(immutable): test immutable functionality

---
+++ test/immutable_spec.js
@@ -0,0 +1,70 @@
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+describe('immutability', () => {
+  describe('a number', () => {
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(state).to.eq(42);
+      expect(nextState).to.eq(43);
+    });
+  });
+
+  describe('a list', () => {
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(state).to.eq(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+
+      expect(nextState).to.eq(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+    });
+  });
+
+  describe('a tree', () => {
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(state).to.eq(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+      expect(nextState).to.eq(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+    });
+  });
+});
