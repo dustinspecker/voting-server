212c8741ce8e87872884b6f496d7d148a99ac3f4
Dustin Specker <dustinspecker@dustinspecker.com>
2015-10-06 18:05:58 -0500

    refactor(immutable_spec): use Map.update

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -40,10 +40,7 @@ describe('immutability', () => {
 
   describe('a tree', () => {
     function addMovie(currentState, movie) {
-      return currentState.set(
-        'movies',
-        currentState.get('movies').push(movie)
-      );
+      return currentState.update('movies', movies => movies.push(movie));
     }
 
     it('is immutable', () => {
