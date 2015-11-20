808a6af8b0a0830b35cae97c2a777dea79385406
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-19 18:10:33 -0600

    feat(server): load initial entries

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ import startServer from './src/server';
 export const store = makeStore();
 
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
